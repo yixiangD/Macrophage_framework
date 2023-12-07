4c3866d73c42a722fcd38cee669dd6ac40ceca7c
yixiangD <yxdeng05@gmail.com>
2023-12-07 14:24:43 -0500

    add examples to read *txt.gz

--- R/read_data.R
+++ R/read_data.R
@@ -1,6 +1,9 @@
 # read sc RNA seq data
 
 sc.dir <- list.dirs(path = "./data", full.names = TRUE, recursive = FALSE)
+
+# scRNA_seq_data now contains your data frame
+
 for (d in sc.dir) {
   files <- list.files(d)
   # print(d)
@@ -15,3 +18,10 @@ for (d in sc.dir) {
     print(d)
   }
 }
+# Path to your .txt.gz file
+file_path <- "/Users/yd973/Documents/research/Macrophage_framework/data/GSE144707/GSM4294086_countTable_nerveStSt.txt.gz"
+
+# Reading the gzipped file directly
+scRNA_seq_data <- read.table(gzfile(file_path), header = TRUE, sep = "\t")
+seurat_obj <- Seurat::CreateSeuratObject(counts = scRNA_seq_data, project = "test")
+seurat_obj <- subset(seurat_obj, subset = nFeature_RNA > 200)
