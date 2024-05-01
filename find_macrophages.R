6d9c753bbfe0bbeba5731819e181cede8be9e090
yixiangD <yxdeng05@gmail.com>
2024-05-01 18:48:21 -0400

    add manual and automate find macrophages

--- find_macrophages.R
+++ find_macrophages.R
@@ -4,114 +4,103 @@ remote.path <-
 local.path <-
   "/Users/yd973/Documents/research/Macrophage_framework/data"
 saved.dir <- "~/Downloads/macrophage"
+test.folder <- c("GSE171330")
 # Load necessary libraries
 library(Seurat) # or another scRNA-seq analysis package
 library(dplyr)
 library(harmony)
 library(ggplot2)
-
-library(celldex)
 library(SingleR)
 library(BiocParallel)
-# find markers----
-# manual
-# markers <- FindConservedMarkers(obj.final, ident.1 = 6, grouping.var = "condition", verbose = FALSE)
-# macropage features
-test.folder <- c("GSE171330")
-for (folder in test.folder) {
-  fname.integrated <- paste(saved.dir, paste(folder, "integrated.RDdata", sep = "_"), sep = "/")
-  obj.final <- readRDS(file = fname.integrated)
-  features <-
-    c("Adgre1", "Csf1r", "H2-Ab1", "Cd68", "Lyz2", "Itgam", "Mertk")
-  feat.plot <- FeaturePlot(obj.final, features = features)
-  
-  ggsave(
-    paste(saved.dir, paste0(
-      paste(folder, "feat.plot", sep = "_"), ".pdf"
-    ), sep = "/"),
-    feat.plot,
-    width = 10,
-    height = 5
-  )
-  
-  obj.small <-
-    subset(
-      obj.final,
-      Adgre1 > 1 |
-        Csf1r > 1 |
-        `H2-Ab1` > 1 | Cd68 > 1 | Lyz2 > 1 | Itgam > 1 | Mertk > 1
-    )
-  obj.small <- RunUMAP(obj.small, reduction = "harmony", dims = 1:30)
-  obj.small <-
-    FindNeighbors(obj.small, reduction = "harmony", dims = 1:30) %>% FindClusters()
-  
-  dim.plot.small <-
-    DimPlot(obj.small,
-            group.by = c("condition", "ident"),
-            ncol = 2
-    )
-  ggsave(
-    paste(saved.dir, paste0(
-      paste(folder, "dim.plot.small", sep = "_"), ".pdf"
-    ), sep = "/"),
-    dim.plot.small,
-    width = 10,
-    height = 5
-  )
-  
+
+opt.method <- "manual"
+# features/genes to plot
+feat.ilya <- c("Lyve1", "Fcgr1")
+feat.paper <- c("Adgre1")
+feat.m1 <- c("Itgam", "Nos2")
+feat.m2 <- c("Mrc1", "Arg1", "Chil3", "Retnla")
+features <- c(feat.ilya, feat.paper, feat.m1, feat.m2)
+
+# features <- c("Adgre1", "Csf1r", "H2-Ab1", "Cd68", "Lyz2", "Itgam", "Mertk")
+
+if (opt.method != "manual") {
   ref <- celldex::MouseRNAseqData()
+}
+
+for (folder in test.folder) {
+  fname.integrated <-
+    paste(saved.dir,
+      paste(folder, "integrated.RDdata", sep = "_"),
+      sep = "/"
+    )
+  obj.final <- readRDS(file = fname.integrated)
   obj.final <- JoinLayers(obj.final)
+  obj.final <- NormalizeData(obj.final, verbose = TRUE)
+  # Retrieve normalized data
+  normalized_data <-
+    GetAssayData(obj.final, assay = "RNA", layer = "data")
   sceObject <- as.SingleCellExperiment(obj.final)
-  
-  singleRResults <-
-    SingleR(
-      test = sceObject,
-      ref = ref,
-      labels = ref$label.main,
-      clusters = obj.final$seurat_clusters
-    )
-  # Viewing the first few annotations
-  head(singleRResults$labels)
-  # Summary of predicted cell types
-  table(singleRResults$labels)
-  
-  scores <- as.data.frame(singleRResults@listData$scores)
-  
-  score.heatmap <-
-    plotScoreHeatmap(
-      singleRResults,
-      clusters = singleRResults$labels,
-      fontsize.row = 9,
-      show_colnames = T
-    )
-  ggsave(
-    paste(saved.dir, paste0(
-      paste(folder, "score.heatmap", sep = "_"), ".pdf"
-    ), sep = "/"),
-    score.heatmap,
-    width = 8,
-    height = 6
-  )
-  
-  new.cluster.ids <- singleRResults$pruned.labels
-  names(new.cluster.ids) <- levels(obj.final)
-  levels(obj.final)
-  obj.final <- RenameIdents(obj.final, new.cluster.ids)
-  
-  umap.integrated <-
-    UMAPPlot(
-      object = obj.final,
-      pt.size = 0.5,
-      label = TRUE
-    )
-  print(umap.integrated)
-  
-  ggsave(
-    paste(saved.dir, paste0(
-      paste(folder, "clusterUMAP", sep = "_"), ".pdf"
-    ), sep = "/"),
-    umap.integrated,
-    width = 6,
-    height = 5
-  )
+
+  if (opt.method == "manual") {
+    # opt1. manually find macrophages----
+    # Check expression of typical macrophage function genes
+    DotPlot(obj.final, features = features)
+
+    feat.avail <- features[features %in% rownames(obj.final)]
+    # Normalize data if not already normalized
+
+    expression_above_threshold <- normalized_data[feat.avail, ] > 1
+    cells_to_keep <- apply(expression_above_threshold, 2, any)
+
+    # Subset the Seurat object to include only cells where all specified genes are above the threshold
+    obj.manual <- obj.final[, cells_to_keep]
+    VlnPlot(obj.manual, features = feat.avail)
+    DotPlot(obj.manual, features = feat.avail)
+    # export to temporary data
+    fname.macro <-
+      paste(saved.dir,
+        paste(folder, "manual_macrophage.RDdata", sep = "_"),
+        sep = "/"
+      )
+    saveRDS(obj.manual, file = fname.macro)
+  } else {
+    # opt2. automate finding macrophages----
+    singleRResults <-
+      SingleR(
+        test = sceObject,
+        ref = ref,
+        labels = ref$label.main,
+        clusters = obj.final$seurat_clusters
+      )
+    # Viewing the first few annotations
+    head(singleRResults$labels)
+    # Summary of predicted cell types
+    table(singleRResults$labels)
+
+    scores <- as.data.frame(singleRResults@listData$scores)
+    # plot
+    score.heatmap <-
+      plotScoreHeatmap(singleRResults,
+        # clusters = singleRResults$labels,
+        fontsize.row = 9,
+        show_colnames = T
+      )
+
+    # extract macrophages only
+    # Subset for a specific cluster, e.g., cluster 0
+    head(obj.final@meta.data)
+
+    # Subset for a specific annotated cell type
+    obj.final$SingleR.labels <- singleRResults$labels
+    obj.auto <-
+      subset(obj.final, subset = SingleR.labels == "Monocytes")
+
+    # export to temporary data
+    fname.macro <-
+      paste(saved.dir,
+        paste(folder, "auto_macrophage.RDdata", sep = "_"),
+        sep = "/"
+      )
+    saveRDS(obj.auto, file = fname.macro)
+  }
 }
